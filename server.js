f4ad4ec56545cf49f70a90a8e4a63909350b0507
Srichakradhar Reddy Nagireddy <srichakra3nsr3@gmail.com>
2016-11-21 09:18:46 +0000

    Add Access-Control-Allow-Origin

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var app = express();
 
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token');
+     // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+});
+
 app.get('/', function (req, res) {
   res.send("...Chuck's IP address, language and operating system Server...");
 });
